56d7480ebd469f5b92dfcd209b897a9c6aacfb83
so0352 <sosa035204@gmail.com>
2024-10-28 15:47:43 +0900

    update evaluation_matrix

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from utils.Board import Board
 def main():
     print("オセロ対戦へようこそ!")
+    return
 
 if __name__ == "__main__":
     bd = Board()
     print(bd.show())
     result = bd.check_flip(2, 3, False)
     print(result)
+    print(bd.evaluation_matrix)
