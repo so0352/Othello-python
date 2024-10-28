cea1d862f430793ede800d69b556d2605f5a1279
so0352 <sosa035204@gmail.com>
2024-10-28 23:32:42 +0900

    make main playing

--- algorithm/max_frip.py
+++ algorithm/max_frip.py
@@ -8,15 +8,15 @@ sys.path.append(project_root)
 from utils.Board import Board
 
 def max_frip(board: Board, available_coordinates_list: list, first_move: bool):
-    max_frip = 0
+    max_frip_num = 0
     max_x = -1
     max_y = -1
-    max_frip = -1
+    max_frip_num = -1
     for (y, x) in available_coordinates_list:
         flip_list = board.check_flip(y, x, first_move) # ひっくり返すことができる方向と個数
         for (dy, dx), num in flip_list:
-            if num > max_frip:
-                max_frip = num
+            if num > max_frip_num:
+                max_frip_num = num
                 max_x = x
                 max_y = y
-    return (max_y, max_x), max_frip
+    return (max_y, max_x), max_frip_num
--- main.py
+++ main.py
@@ -1,22 +1,116 @@
 from utils.Board import Board
 from algorithm.max_frip import max_frip
+import sys
+
+
+## 座標を受け取って盤面に適用する関数
+def input_coordinate(board: Board, first_move: bool):
+    while True:
+        print("置きたい場所の座標をスペース区切りで入力してください.入力は半角.")
+        print("値は0~7の整数で入力")
+        try:
+            y, x = map(int, input("縦 横 \n").split())
+            # 指定した範囲内の座標なら
+            if 0 <= y < 8 and 0 <= x < 8:
+                add_return = board.add_piece(y, x, first_move)
+                if add_return == False:
+                    print("エラー: その場所には駒を置けません。")
+                    continue
+                else:
+                    print("駒を置きました。")
+                    board.show()
+                    break
+            else:
+                print("エラー: 0~7の整数で入力してください。")
+                continue
+            
+        except ValueError:
+            print("エラー: スペースで区切った2つの数字を入力してください。")
+        except KeyboardInterrupt:
+            print("\nプログラムを終了します。")
+            sys.exit()
+
+
 def main():
-    print("オセロ対戦へようこそ!")
-    return
+    print("オセロゲームを開始します．")
+    first_move = True if int(input("先手なら0,後手なら1を入力してください. \n")) == 0 else False
+    board = Board(first_move=first_move)
+    board.show()
 
+    # 人間が先手なら
+    if first_move:
+        while True:
+            # 人間側
+            if board.show_available_coordinates(first_move) == []:
+                print("あなたはパスします.")
+                first_move = False # AIの手番に移る
+            else:   
+                input_coordinate(board=board, first_move=first_move)
+                first_move = False # AIの手番に移る
+            # AI側
+            (max_y, max_x), max_frip_num = max_frip(board, board.show_available_coordinates(first_move), first_move) # 最適手を取得
+            if max_frip_num == 0:
+                print("AIはパスします.")
+                first_move = True # 人間の手番に移る
+            else:
+                print(f"AIの手: {max_y} {max_x}", f"ひっくり返す数: {max_frip_num}")
+                if max_frip_num == -1:
+                    print("AIはパスします.")
+                    first_move = True # 人間の手番に移る
+                else:
+                    add_return = board.add_piece(max_y, max_x, first_move)
+
+                    if add_return == False:
+                        print("エラー: AIの手が正しくありません.")
+                        sys.exit()
+                    else:
+                        print("AIの手を置きました.")
+                        board.show()
+                        first_move = True # 人間の手番に移る
+
+            # ゲーム終了判定
+            if board.finished:
+                board.show()
+                print("ゲーム終了")
+                break
+    
+    # AIが先手なら   
+    elif first_move == False:
+        while True:
+            # AI側
+            (max_y, max_x), max_frip_num = max_frip(board, board.show_available_coordinates(first_move), first_move) # 最適手を取得
+            if max_frip_num == 0:
+                print("AIはパスします.")
+                first_move = True # 人間の手番に移る
+            else:
+                print(f"AIの手: {max_y} {max_x}", f"ひっくり返す数: {max_frip_num}")
+                if max_frip_num == -1:
+                    print("AIはパスします.")
+                    first_move = True
+                else:
+                    add_return = board.add_piece(max_y, max_x, first_move)
+                    if add_return == False:
+                        print("エラー: AIの手が正しくありません.")
+                        sys.exit()
+                    else:
+                        print("AIの手を置きました.")
+                        board.show()
+                        first_move = True # 人間の手番に移る
+            
+            # 人間側
+            if board.show_available_coordinates(first_move) == []:
+                print("あなたはパスします.")
+                first_move = False # AIの手番に移る
+            else:   
+                input_coordinate(board=board, first_move=first_move)
+                first_move = False # AIの手番に移る
+            
+            # ゲーム終了判定
+            if board.finished:
+                board.show()
+                print("ゲーム終了")
+                break
+
+    sys.exit()
 if __name__ == "__main__":
-    bd = Board()
-    print(bd.show())
-    first_move = True
-    #if bd.add_piece(2, 3, first_move):
-    #   pass
-    #else:
-    #    print("駒を置くことができません。")
-    #
-    #print(bd.show())
-    print(bd.show_available_coordinates(first_move))
-
-
-    tp ,numss = max_frip(bd, [(2, 4), (3, 5), (4, 2), (5, 3)], first_move)
-
-    print(tp, numss)
+    main()
